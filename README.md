fbb87978fbb2e7169da470218fdd1d3a5529ee8a
Daniel Alexander Vasilyonok <danielv@bu.edu>
2018-07-14 21:35:00 +0000

    1st commit and initial files for asynch chatting app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project 2
+
+Web Programming with Python and JavaScript
---
+++ application.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+# list of all channels
+channel_list = ['general']
+
+@app.route("/")
+def index():
+    return render_template("index.html")
